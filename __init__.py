87a6aa9ffb3ca11c905f0751b5006dacc32dd789
easonturing <715812008@qq.com>
2020-10-27 15:15:34 +0800

    First Commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/27 14:39
+# @Author  : Chenyiming
+
---
+++ applications/pos_tagger.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/27 14:48
+# @Author  : Chenyiming
+
